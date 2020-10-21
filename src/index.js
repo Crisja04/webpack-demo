d4f0c2c9aaedb5f151c3a447ae276cd0e4fa8cb7
Cristian Tavarez <cristian.javier.11@hotmail.com>
2020-10-21 17:55:04 -0400

    web server cleanup

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
 import _ from 'lodash';
-import toml from './data/data.toml';
-import yaml from './data/data.yaml';
-import json from './data/data.json5';
-import printMe from './print.js';
+
 
 function component() {
     const element = document.createElement('div');
-    const btn = document.createElement('button');
   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
-    // Json files data output
-    console.log(toml.title);
-    console.log(toml.owner.name);
-
-    console.log(yaml.title);
-    console.log(yaml.owner.name);
-
-    console.log(json.title);
-    console.log(json.owner.name);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,6 @@ const path = require('path');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const ASSET_PATH = process.env.ASSET_PATH || '/'; // use in public path for terser
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
