7549d1161e4e03eb6732cca3f4cb2cf80d0736c9
Cristian Tavarez <cristian.javier.11@hotmail.com>
2020-10-21 13:53:26 -0400

    asset management cleanup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data/data.xml';
-import Notes from './data/data.csv';
 import toml from './data/data.toml';
 import yaml from './data/data.yaml';
 import json from './data/data.json5';
@@ -10,18 +6,7 @@ import json from './data/data.json5';
 function component() {
     const element = document.createElement('div');
   
-    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-    // Data output
-    console.log(Data);
-    console.log(Notes);
 
     // Json files data output
     console.log(toml.title);
--- webpack.config.js
+++ webpack.config.js
@@ -17,34 +17,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: [
-          'csv-loader',
-        ],
-      },
-      {
-        test: /\.xml$/,
-        use: [
-          'xml-loader',
-        ],
-      },
-      {
         test: /\.toml$/,
         type: 'json',
         parser: {
