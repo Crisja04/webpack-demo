f888932f00e10c3bf90da1337c7213c604d303b0
Cristian Tavarez <cristian.javier.11@hotmail.com>
2020-10-23 20:30:30 -0400

    PWA service workers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import _ from 'lodash';
 import printMe from './print';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+
 function component() {
   const element = document.createElement('div');
-  const btn = document.createElement('button');
+ const btn = document.createElement('button');
 
-  element.innerHTML = join(['Hello', 'webpack'], ' ');
-  
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  element.appendChild(btn);
-  
+ btn.innerHTML = 'Click me and check the console!';
+ btn.onclick = printMe;
+
+ element.appendChild(btn);
+
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -15,7 +15,13 @@ module.exports = {
     // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-     title: 'Output Management',
+     title: 'Progressive Web Application',
+    }),
+    new WorkboxPlugin.GenerateSW({
+        // these options encourage the ServiceWorkers to get in there fast
+        // and not allow nay strangling "old" SWs to hang around
+        clientsClaim: true,
+        skipWaiting: true,
     }),
   ],
   output: {
